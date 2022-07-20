c110ee751493adf3da5322dac17824ad4c0437c8
sanghun <2002lkw@naver.com>
2022-07-20 23:39:18 +0900

    코딩테스트때 쓸만한것들 추가

--- src/main/java/com/cdg/App.java
+++ src/main/java/com/cdg/App.java
@@ -11,6 +11,20 @@ public class App
     public static void main( String[] args )
     {
         boolean contains = StringUtils.contains("안녕하세요", "요");
-        System.out.println(contains );
+        System.out.println(contains);
+
+        String differ = StringUtils.difference("123456","129456");
+        System.out.println(differ);
+
+        String getDigit = StringUtils.getDigits("123ddwad456");
+        System.out.println(getDigit);
+
+        System.out.println(StringUtils.defaultString(null));
+
+        String remove = StringUtils.remove("가나가다라가가마바사","가");
+        System.out.println(remove);
+
+
+
     }
 }
